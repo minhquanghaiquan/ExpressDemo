3f49cd3ef8e30c3a030270b9017d6a830bc5d081
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-05-19 10:47:00 +0200

    middleware demo

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var validate = require('../validate/user.validate');
 
 var controller=require('../controllers/user.controller');
 // /* file db */
@@ -18,12 +19,14 @@ var controller=require('../controllers/user.controller');
 
 router.get('/', controller.index);
 
+
+
 router.get('/search', controller.search);
 
 router.get('/create' , controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create',validate.postCreate, controller.postCreate);
 
 module.exports = router;
